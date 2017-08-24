ceb6b6fdcbd46e03f14342dac2cf7957093eedb3
salamflamo <untungsuprapto69@gmail.com>
2017-08-25 00:18:28 +0700

    Memperbaiki kesalahan

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $encrypted_469002a8da83_key -iv $encrypted_469002a8da83_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
+cd /home/fans17/
+git clone git@github.com:salamflamo/trytravis.git
 
 
 
